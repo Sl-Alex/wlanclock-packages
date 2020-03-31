5a68d35384b6a9710667e6a15215ce68879f8f3f
Oleksii Slabchenko <o.slabchenko@reply.de>
2020-03-31 19:55:24 +0200

    Report sensors data to wlanclock-app over ubus.

--- wlanclock-sensors/files/src/main.c
+++ wlanclock-sensors/files/src/main.c
@@ -10,6 +10,8 @@
 #include "bh1750.h"
 #include "bme280_wrapper.h"
 
+#include "ubus_client.h"
+
 int main(void)
 {
     char const *i2cdev = "/dev/i2c-0";
@@ -17,40 +19,48 @@ int main(void)
 
     int f = i2c_init(i2cdev);
 
+    /* Initialize BH1750 */
     rc = bh1750_init();
     if (rc != 0)
     {
         fprintf(stderr, "Can't initialize BH1750\n");
     }
 
+    /* Wait a bit until measurement results are available */
     usleep(200000);
 
-    uint16_t brightness;
-    rc = bh1750_read(&brightness);
-    if (rc != 0)
+    while(1)
     {
-        fprintf(stderr, "Can't read brightness\n");
-    }
-    else
-    {
-        printf("Brightness = 0x%04X\n", brightness);
-    }
+        uint16_t brightness;
+        float temp, hum, press;
+
+        int rc_bh1750 = bh1750_read(&brightness);
+        if (rc_bh1750 != 0)
+        {
+            fprintf(stderr, "Can't read brightness\n");
+        }
 
 
-    bme280_wrap_init();
+        int rc_bme280 = bme280_wrap_init();
+        rc_bme280 |= bme280_wrap_get_data(&temp, &hum, &press);
 
-    float temp, hum, press;
-    rc = bme280_wrap_get_data(&temp, &hum, &press);
-    if (rc != 0)
-    {
-        fprintf(stderr, "Failed to stream sensor data (code %+d).\n", rc);
-    }
-    else
-    {
-        printf("%0.2lf deg C, %0.2lf%%, %0.2lf hPa\n", temp, hum, press);
+        if (rc_bme280 != 0)
+        {
+            fprintf(stderr, "Failed to get data (code %+d).\n", rc);
+        }
+
+        if (!rc_bh1750  && !rc_bme280)
+        {
+            ubus_client_send(brightness, press, hum, temp);
+        }
+
+        /* Sleep for 1 second */
+        usleep(1000000);
     }
 
     close(f);
 
     printf("Done\n");
+
+    return 0;
 }
